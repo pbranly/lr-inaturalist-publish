36fafe00cd87e40b63c04bab38e13bc46b267802
Philippe <philippe.branly@gmail.com>
2025-10-09 18:16:33 +0200

    correct white spaces

--- lr-inaturalist-publish.lrdevplugin/InitPlugin.lua
+++ lr-inaturalist-publish.lrdevplugin/InitPlugin.lua
@@ -1,6 +1,8 @@
 local logger = import("LrLogger")("lr-inaturalist-publish")
 local LrTasks = import("LrTasks")
+
 local prefs = import("LrPrefs").prefsForPlugin()
+
 local function configureLogging(_, _, value)
 	logger:disable()
 	if value == "trace" then
@@ -8,9 +10,11 @@ local function configureLogging(_, _, value)
 		logger:info("Enabled trace logging")
 	end
 end
+
 LrTasks.startAsyncTask(function()
 	prefs:addObserver("logLevel", configureLogging)
 	configureLogging(prefs, "logLevel", prefs.logLevel)
 	logger:trace("--------------- Starting iNaturalist Publish Service Plugin")
+		
 	require("Updates").check(false)
 end)
