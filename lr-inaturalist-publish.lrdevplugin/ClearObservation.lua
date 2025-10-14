4642a574d4040b39b4df7131ab51f9e77e86cbec
Philippe <philippe.branly@gmail.com>
2025-10-14 17:30:29 +0200

    internationalisations

--- lr-inaturalist-publish.lrdevplugin/ClearObservation.lua
+++ lr-inaturalist-publish.lrdevplugin/ClearObservation.lua
@@ -1,33 +1,29 @@
 local LrApplication = import("LrApplication")
 local LrDialogs = import("LrDialogs")
-
 local MetadataConst = require("MetadataConst")
 
 -- Clear the observation UUID field on selected photos
 local function clearObservation()
-    local catalog = LrApplication.activeCatalog()
-    local photos = catalog:getTargetPhotos()
-    local confirmation = LrDialogs.confirm(
-        LOC("$$$/iNat/Clear/DeleteObservationData=Delete the observation data from ^1 photos?", #photos),
-        LOC("$$$/iNat/Clear/DeleteObservationData/Desc=This will clear the observation UUID and " ..
-            "URL metadata fields from these photos")
-    )
-
-    if confirmation == "cancel" then
-        return
-    end
-
-    catalog:withWriteAccessDo("Clear observation", function(_)
-        for _, photo in pairs(photos) do
-            photo:setPropertyForPlugin(_PLUGIN, MetadataConst.ObservationUUID, nil)
-            photo:setPropertyForPlugin(_PLUGIN, MetadataConst.ObservationURL, nil)
-        end
-        local msg = LOC("$$$/iNat/Clear/RemovedObservation=Removed observation metadata from ^1 photos", #photos)
-        if #photos == 1 then
-            msg = LOC("$$$/iNat/Clear/RemovedObservationSingle=Removed observation metadata from ^1 photo", #photos)
-        end
-        LrDialogs.showBezel(msg)
-    end)
+	local catalog = LrApplication.activeCatalog()
+	local photos = catalog:getTargetPhotos()
+	local confirmation = LrDialogs.confirm(
+		LOC("$$$/iNat/Clear/DeleteObservationData=Delete the observation data from ^1 photos?", #photos),
+		LOC("$$$/iNat/Clear/DeleteObservationData/Detail=This will clear the observation UUID and URL metadata fields from these photos")
+	)
+	if confirmation == "cancel" then
+		return
+	end
+	catalog:withWriteAccessDo("Clear observation", function(_)
+		for _, photo in pairs(photos) do
+			photo:setPropertyForPlugin(_PLUGIN, MetadataConst.ObservationUUID, nil)
+			photo:setPropertyForPlugin(_PLUGIN, MetadataConst.ObservationURL, nil)
+		end
+		local msg = LOC("$$$/iNat/Clear/RemovedObservationMetadata=Removed observation metadata from ^1 photos", #photos)
+		if #photos == 1 then
+			msg = LOC("$$$/iNat/Clear/RemovedObservationMetadataSingular=Removed observation metadata from ^1 photo", #photos)
+		end
+		LrDialogs.showBezel(msg)
+	end)
 end
 
 import("LrTasks").startAsyncTask(clearObservation)
